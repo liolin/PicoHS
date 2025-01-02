950e74750cfa79925fb324b35c632d498663b057
Olivier Lischer <olivier.lischer@ost.ch>
2025-01-02 15:23:56 +0100

    Clean up MicroHs configuration.

--- runtime/config-raspberry-pico.h
+++ runtime/config-raspberry-pico.h
@@ -6,24 +6,21 @@
 #include <stdio.h>
 
 /*
- * Include stdio functions.
- * Without this none of the file I/O in System.IO is available.
+ * Do not include STDIO as we do not have a file system on the Pico.
  */
 #define WANT_STDIO 0
 
+/*
+ * Do not include directory operations as we do not have a file system on the
+ * Pico.
+ */
+#define WANT_DIR 0
+
+/*
+ * Do not process argc and argv. We have no way of processing them on the Pico
+ * anyway.
+ */
 #define WANT_ARGS 0
-
-/*
- * Include ops for floating point arithmetic.
- * Without this +,-,* etc will not be available for the Double type.
- */
-#define WANT_FLOAT 1
-
-/*
- * Include <math.h>
- * Without this, exp,sin, etc are not available.
- */
-#define WANT_MATH 1
 
 /*
  * Include MD5 checksumming code
@@ -31,62 +28,48 @@
 #define WANT_MD5 1
 
 /*
- * Include profiling code
+ * I do not need profiling (and otherwise causes problems with WANT_STDIO 0).
  */
 #define WANT_TICK 0
 
-/*
- * Include time_t type
- */
+#define WANT_FLOAT 1
+#define WANT_MATH 1
 #define WANT_TIME 1
 
 /*
- * Include directory manipulation
- */
-#define WANT_DIR 0
-
-/*
  * Find First Set
- * This macro must be defined.
  * It return the number of the least significant bit that is set.
  * Numberings starts from 1.  If no bit is set, it should return 0.
+ * GCC has ffsl as a builtin.
  */
-/* GCC has ffsl as a builtin. */
 #define FFS __builtin_ffsl
-
-
-/*
- * This is the character used for comma-separation in printf.
- * Defaults to "'".
- */
-/* #define PCOMMA "'" */
-
-/*
- * Get a raw input character.
- * If undefined, the default always returns -1
- */
-/* #define GETRAW */
-
-/*
- * Get time since some epoch in milliseconds.
- */
-/* #define GETTIMEMILLI */
 
 /*
  * The ERR macro should report an error and exit.
- * If not defined, a generic one will be used.
+ * It is the same macro as the standard macro, but writes to STDERR, although
+ * WANT_STDIO is set to 0, because we have STDIO / STDERR available.
  */
-#define ERR(s)    do { fprintf(stderr,"ERR: "s"\n");   EXIT(1); } while(0)
-#define ERR1(s,a) do { fprintf(stderr,"ERR: "s"\n",a); EXIT(1); } while(0)
+#define ERR(s)                                                                 \
+  do {                                                                         \
+    fprintf(stderr, "ERR: "s                                                   \
+                    "\n");                                                     \
+    EXIT(1);                                                                   \
+  } while (0)
+#define ERR1(s, a)                                                             \
+  do {                                                                         \
+    fprintf(stderr,                                                            \
+            "ERR: "s                                                           \
+            "\n",                                                              \
+            a);                                                                \
+    EXIT(1);                                                                   \
+  } while (0)
 
-
-#define GCRED    1 /* do some reductions during GC */
+#define GCRED 1    /* do some reductions during GC */
 #define FASTTAGS 1 /* compute tag by pointer subtraction */
 #define INTTABLE 1 /* use fixed table of small INT nodes */
-#define SANITY   1 /* do some sanity checks */
+#define SANITY 1   /* do some sanity checks */
 #define STACKOVL 1 /* check for stack overflow */
 
-// These values work for the simplified version
 #define HEAP_CELLS 12000
 #define STACK_SIZE 500
 
@@ -103,7 +86,8 @@ void main_setup(void) {
 
 void myexit(int n) {
   printf("Finished with code: %d\n", n);
-  while (1) {}
+  while (1) {
+  }
 }
 #define EXIT myexit
 
